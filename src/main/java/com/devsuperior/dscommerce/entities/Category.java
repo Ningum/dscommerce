685eccf746f14b61f5440ddf47e245e14168f34b
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-05-28 19:37:23 -0300

    Criação das classes Category e Product

--- src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -4,13 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "tb_category")
 public class Category {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   private String name;
 
@@ -35,5 +43,9 @@ public class Category {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Set<Product> getProducts() {
+    return products;
   }
 }
--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -1,11 +1,19 @@
 package com.devsuperior.dscommerce.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "tb_product")
 public class Product {
 
   @Id
@@ -13,9 +21,20 @@ public class Product {
   private Long id;
 
   private String name;
+
+  @Column(columnDefinition = "TEXT")
   private String description;
+
   private Double price;
   private String imgUrl;
+
+  @ManyToMany
+  @JoinTable(
+    name = "tb_product_category",
+    joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+  )
+  private Set<Category> categories = new HashSet<>();
 
   public Product() {}
 
@@ -71,5 +90,9 @@ public class Product {
 
   public void setImgUrl(String imgUrl) {
     this.imgUrl = imgUrl;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
   }
 }
