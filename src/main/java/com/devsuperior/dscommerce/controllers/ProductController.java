ee1f57c3b92190bce1cc1e95f52a5afaedb2ddf1
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-06-05 07:47:58 -0300

    Inplementação da busca por produtos paginada

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,11 @@ public class ProductController {
 
   @Autowired
   private ProductService service;
+
+  @GetMapping
+  public Page<ProductDTO> findAll(Pageable pageable) {
+    return service.findaAll(pageable);
+  }
 
   @GetMapping(value = "/{id}")
   public ProductDTO findById(@PathVariable Long id) {
