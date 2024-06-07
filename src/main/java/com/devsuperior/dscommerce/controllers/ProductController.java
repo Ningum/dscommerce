77e6ec66eb02722eb5c6ef0f19396aa5515664fb
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-06-07 07:32:38 -0300

    Implementação de validacão para os metodos insert e update de produtos

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -2,6 +2,9 @@ package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+
+import jakarta.validation.Valid;
+
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -37,7 +40,7 @@ public class ProductController {
   }
 
   @PostMapping
-  public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+  public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
     dto = service.insert(dto);
     URI uri = ServletUriComponentsBuilder
       .fromCurrentRequest()
@@ -50,7 +53,7 @@ public class ProductController {
   @PutMapping(value = "/{id}")
   public ResponseEntity<ProductDTO> update(
     @PathVariable Long id,
-    @RequestBody ProductDTO dto
+    @Valid @RequestBody ProductDTO dto
   ) {
     dto = service.update(id, dto);
     return ResponseEntity.ok(dto);
