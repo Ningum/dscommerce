dea13e6b39f4e143a6f9e0ec0904c2257737fd33
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-07-23 21:54:27 -0300

    Iniciando a inclusao de seguranca ao projeto e mudanca no seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -5,11 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -30,6 +35,14 @@ public class User {
 
   @OneToMany(mappedBy = "client")
   private List<Order> orders = new ArrayList<>();
+
+  @ManyToMany
+  @JoinTable(
+    name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
+  private Set<Role> roles = new HashSet<>();
 
   public User() {}
 
@@ -99,6 +112,23 @@ public class User {
 
   public List<Order> getOrders() {
     return orders;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void addRole(Role role) {
+    roles.add(role);
+  }
+
+  public boolean hasRole(String roleName) {
+    for (Role role : roles) {
+      if (role.getAuthority().equals(roleName)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @Override
