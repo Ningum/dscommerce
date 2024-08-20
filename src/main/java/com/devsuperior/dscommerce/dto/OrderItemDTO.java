a2acca51b12b80e942a0a65a10a87551d8fbf172
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-08-20 06:26:44 -0300

    Incluindo a imagem do produto no orderItem

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,17 +8,20 @@ public class OrderItemDTO {
   private String name;
   private Double price;
   private Integer quantity;
+  private String imgUrl;
 
   public OrderItemDTO(
     Long productId,
     String name,
     Double price,
-    Integer quantity
+    Integer quantity,
+    String imgUrl
   ) {
     this.productId = productId;
     this.name = name;
     this.price = price;
     this.quantity = quantity;
+    this.imgUrl = imgUrl;
   }
 
   public OrderItemDTO(OrderItem entity) {
@@ -26,6 +29,7 @@ public class OrderItemDTO {
     name = entity.getProduct().getName();
     price = entity.getPrice();
     quantity = entity.getQuantity();
+    imgUrl = entity.getProduct().getImgUrl();
   }
 
   public Long getProductId() {
@@ -46,5 +50,9 @@ public class OrderItemDTO {
 
   public Double getSubTotal(){
     return price * quantity;
+  }
+
+  public String getImgUrl(){
+    return imgUrl;
   }
 }
