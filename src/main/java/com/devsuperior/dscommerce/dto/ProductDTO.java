cb28d9db88c1c739045576e7516a00955ac20cfe
Samuel Ningum Pedroso <samuel-mm@hotmail.com.br>
2024-08-20 20:18:30 -0300

    Correção na validação de produto

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class ProductDTO {
   @NotBlank(message = "Campo requerido")
   private String description;
 
+  @NotNull(message = "Campo Requerido")
   @Positive(message = "O preço deve ser positivo")
   private Double price;
 
